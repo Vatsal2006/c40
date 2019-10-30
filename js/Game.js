87180a4b32a3dfdb9abb79f51c6e32d3ce8604d2
Vatsal2006 <vatsal.kasera@pathwaysschool.in>
2019-10-30 16:44:16 +0530

    rank

--- js/Game.js
+++ js/Game.js
@@ -44,45 +44,31 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    Player.getCarsAtEnd();
+
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
-      
-      //var display_position = 100;
-      
-      //index of the array
-      var index = 0;
 
-      //x and y position of the cars
+      var index = 0;
       var x = 175 ;
       var y;
 
-      for(var plr in allPlayers){
-        //add 1 to the index for every loop
-        index = index + 1 ;
-
-        //position the cars a little away from each other in x direction
-        x = x + 200;
-        //use data form the database to display the cars in y direction
-        y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
-        cars[index-1].y = y;
-       // console.log(index, player.index)
-
-       
-        if (index === player.index){
-          stroke(10);
-          fill("red");
-          ellipse(x,y,60,60);
-          cars[index - 1].shapeColor = "red";
-          camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y;
-        }
-       
-        //textSize(15);
-        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
-      }
+   for(var plr in allPlayers){
+     index = index + 1 ;
+     x = x + 2;
+     y = displayHeight - allPlayers[plr].distance;
+     cars[index-1].x = x;
+     cars[index-1].y = y;
+     if (index === player.index){
+       stroke(10);
+       fill("red");
+       ellipse(x,y,60,60);
+       cars[index - 1].shapeColor = "red";
+       camera.position.x = displayWidth/2;
+       camera.position.y = cars[index-1].y;
+     }
+   }
 
     }
 
@@ -91,8 +77,10 @@ class Game {
       player.update();
     }
 
-    if(player.distance > 3860){
+    if(player.distance > 3800){
       gameState = 2;
+    player.rank += 1;
+    Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +88,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
